1b481d87f5dacc75e9b9ff09515704cae27c00bc
zevdvlpr <zevdvlpr@gmail.com>
2021-08-02 22:19:41 -0300

    ✨ feat: app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,19 @@
 import { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+import { theme } from '../styles/theme';
+
+import '../styles/slider-styles.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
