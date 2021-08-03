444266497cc9c4ad7f07c3487de78b8061946b52
zevdvlpr <zevdvlpr@gmail.com>
2021-08-02 22:20:00 -0300

    ✨ feat: home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,47 @@
+import Head from 'next/head';
+import { Flex, Box, Heading } from '@chakra-ui/react';
+
+import { continents } from '../data';
+
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { TravelTypes } from '../components/TravelTypes';
+import { Slider } from '../components/Slider';
+
 export default function Home() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <Head>
+        <title>WorldTrip | Home</title>
+      </Head>
+
+      <Flex direction="column">
+        <Header />
+
+        <Banner />
+        <TravelTypes />
+
+        <Box
+          w={['60px', '90px']}
+          mx="auto"
+          h="2px"
+          bg="gray.700"
+          my={['9', '20']}
+        />
+
+        <Heading
+          textAlign="center"
+          fontWeight="500"
+          mb={['5', '14']}
+          fontSize={['lg', '3xl', '4xl']}
+        >
+          Vamos nessa?
+          <br />
+          Então escolha seu continente
+        </Heading>
+
+        <Slider continents={continents} />
+      </Flex>
+    </>
+  );
 }
