5a5254f14680e263aa796cd9858b39a2a2d58137
zevdvlpr <zevdvlpr@gmail.com>
2021-08-02 22:18:59 -0300

    ✨ feat: continents data

---
+++ src/data/index.ts
@@ -0,0 +1,38 @@
+export const continents = [
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+  {
+    slug: 'europe',
+    name: 'Europa',
+    description: 'O continente mais antigo.',
+    image: '/images/continents/carousel/london.png',
+  },
+];
